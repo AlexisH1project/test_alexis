77520e6c44a00d5706438248323b351d7270379d
AlexisH1project <projects.h1mx@gmail.com>
2022-01-17 22:12:25 -0600

    SMTP

--- roles/Controller/link_confirmacion.php
+++ roles/Controller/link_confirmacion.php
@@ -22,26 +22,14 @@ function enviarCorreo($codigo, $nombre_cl, $correo_cl){
     //This should be done in your php.ini, but this is how to do it if you don't have access to that
     date_default_timezone_set('Etc/UTC');
 
-    $mail = new PHPMailer();
-    //Tell PHPMailer to use SMTP
-    $mail->isSMTP();
-    //Enable SMTP debugging
-    // SMTP::DEBUG_OFF = off (for production use)
-    // SMTP::DEBUG_CLIENT = client messages
-    // SMTP::DEBUG_SERVER = client and server messages
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-    //Set the hostname of the mail server
-    $mail->Host = 'smtp.gmail.com';
-    //Set the SMTP port number - likely to be 25, 465 or 587
-    $mail->Port = 587; //25,465
-    //Whether to use SMTP authentication
-    $mail->SMTPAuth = true;
-    //Username to use for SMTP authentication
-    $mail->Username = 'ximenahernandez422@gmail.com';
-    //Password to use for SMTP authentication
-    $mail->Password = 'Natacion151020';
-    $mail->SMTPSecure = 'tls';
-    //Set who the message is to be sent from
+    $phpmailer = new PHPMailer();
+    $phpmailer->isSMTP();
+    $phpmailer->Host = 'smtp.mailtrap.io';
+    $phpmailer->SMTPAuth = true;
+    $phpmailer->Port = 2525;
+    $phpmailer->Username = '8add26ec7134d2';
+    $phpmailer->Password = '8f6d653a147dec';
+
     $mail->setFrom('ximenahernandez422@gmail.com', 'Aministrador WEPORT');
     //Set an alternative reply-to address
     $mail->addReplyTo('ximenahernandez422@gmail.com', 'Admin');
