ad59771e9e5277ef41528918629827896f1e948b
AlexisH1project <projects.h1mx@gmail.com>
2022-01-18 17:07:46 -0600

    prueba con funcion email

--- roles/Controller/link_confirmacion.php
+++ roles/Controller/link_confirmacion.php
@@ -59,6 +59,24 @@ function enviarCorreo($codigo, $nombre_cl, $correo_cl){
     } else {
         echo "<script>alert('Confirmacion correcta!'); </script>";
     }
+
+     
+    $asunto = "Este mensaje es de prueba"; 
+    $cuerpo = ' 
+    <html> 
+    <head> 
+    <title>Prueba de correo</title> 
+    </head> 
+    <body> 
+    <h1>Hola amigos!</h1> 
+    <p> 
+    <b>Bienvenidos a mi correo electrónico de prueba</b>. Estoy encantado de tener tantos lectores. Este cuerpo del mensaje es del artículo de envío de mails por PHP. Habría que cambiarlo para poner tu propio cuerpo. Por cierto, cambia también las cabeceras del mensaje. 
+    </p> 
+    </body> 
+    </html> 
+    ';
+
+    mail($correo_cl,$asunto,$cuerpo) ;
 }
 
 // validamos que el correo no exista en la DB
